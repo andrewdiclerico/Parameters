c664c0fc89fa5bab27e51ef0a225336972996f41
andrewdiclerico <diclerico.andrew@gmail.com>
2022-10-02 21:01:35 -0400

    fav icon

--- index.php
+++ index.php
@@ -4,15 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="include/style.css"/>
-    <link rel="icon" href="/favicon.ico">
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
     <title>Parameters</title>
 </head>
 <body>
 
+    <!-- Navigation bar -->
+    <nav class="navbar navbar-expand-lg navbar-light">        
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Parameters</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="input.php">New Test</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <a class="nav-link disabled" style="padding-right:2%;">Login</a>
+        </div>
+    </nav>
+
+
+    <!-- Home page information -->
     <div>
-         heyo
+         
+        <h1>
+            Home page information goes here
+        </h1>
 
 
     </div>
