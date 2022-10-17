747f0e839310a42b702c8876d44e944fa0f26568
andrewdiclerico <diclerico.andrew@gmail.com>
2022-10-16 20:22:28 -0400

    changed nav bar to collapse on resize

--- input.php
+++ input.php
@@ -13,7 +13,7 @@
 
     <!-- Navigation bar -->
     <nav class="navbar navbar-expand-lg navbar-light">        
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
                   <a class="nav-link" href="index.php">Parameters</a>
